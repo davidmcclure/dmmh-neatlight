0a94b723d113ffc962700b04a3a989af7063ee1b
David McClure <davidwilliammcclure@gmail.com>
2014-10-09 00:15:16 -0700

    Mouseover dot.

--- neatline/exhibits/themes/minard/src/javascripts/chart/chart.view.js
+++ neatline/exhibits/themes/minard/src/javascripts/chart/chart.view.js
@@ -16,90 +16,170 @@ Neatline.module('Chart', function(Chart) {
 
 
     /**
-     * Construct the SVG container.
+     * Build the graph.
      */
     initialize: function() {
+      this._initGraph();
+      this._initFocus();
+    },
 
-      var m = {
+
+    /**
+     * Construct axes and time-series line.
+     */
+    _initGraph: function() {
+
+      var self = this;
+
+      this.margin = {
         top:    20,
         right:  30,
         bottom: 30,
         left:   50
       };
 
-      var w = 300 - m.left - m.right;
-      var h = 400 - m.top - m.bottom;
+      // Dimensions.
+      this.width = 300 - this.margin.left - this.margin.right;
+      this.height = 400 - this.margin.top - this.margin.bottom;
+
+      // Vertical/horizontal margins.
+      var vMargin = this.margin.top + this.margin.bottom;
+      var hMargin = this.margin.right + this.margin.left;
 
       // Inject the SVG container.
       this.svg = d3.select(this.el)
         .append('svg')
-        .attr('width', w + m.left + m.right)
-        .attr('height', h + m.top + m.bottom)
+        .attr('width', this.width + hMargin)
+        .attr('height', this.height + vMargin)
         .append('g')
-        .attr('transform', 'translate('+m.left+','+m.top+')');
+        .attr('transform', 'translate('+
+          this.margin.left+','+this.margin.top+
+        ')');
 
       // X-axis scale.
-      var x = d3.time.scale()
-        .range([0, w]);
+      this.xScale = d3.time.scale()
+        .range([0, this.width]);
 
       // Y-axis scale.
-      var y = d3.scale.linear()
-        .range([h, 0]);
+      this.yScale = d3.scale.linear()
+        .range([this.height, 0]);
 
       // X-axis.
-      var xAxis = d3.svg.axis()
-        .scale(x)
+      this.xAxis = d3.svg.axis()
+        .scale(this.xScale)
         .orient('bottom')
         .tickFormat(d3.time.format('%b'));
 
       // Y-axis.
-      var yAxis = d3.svg.axis()
-        .scale(y)
+      this.yAxis = d3.svg.axis()
+        .scale(this.yScale)
         .orient('left')
         .tickFormat(function(t) {
           return (t/1000)+'k';
         });
 
       // Line builder.
-      var line = d3.svg.line()
+      this.line = d3.svg.line()
         .x(function(d) {
-          return x(d[0]);
+          return self.xScale(d[0]);
         })
         .y(function(d) {
-          return y(d[1]);
-        })
+          return self.yScale(d[1]);
+        });
 
       // ISO8601 parser.
       var parseDate = d3.time.format('%Y-%m-%d').parse;
 
       // Parse the dates.
-      var data = _.map(Chart.data, function(d) {
+      this.data = _.map(Chart.data, function(d) {
         return [parseDate(d[0]), d[1]];
       });
 
       // X-axis bounds.
-      x.domain(d3.extent(data, function(d) {
+      this.xScale.domain(d3.extent(this.data, function(d) {
         return d[0];
       }));
 
       // Y-axis bounds.
-      y.domain(d3.extent(data, function(d) {
+      this.yScale.domain(d3.extent(this.data, function(d) {
         return d[1];
       }));
 
+      // Render the X-axis.
       this.svg.append('g')
         .attr('class', 'x axis')
-        .attr('transform', 'translate(0,'+h+')')
-        .call(xAxis)
+        .attr('transform', 'translate(0,'+this.height+')')
+        .call(this.xAxis)
 
+      // Render the Y-axis.
       this.svg.append('g')
         .attr('class', 'y axis')
-        .call(yAxis);
+        .call(this.yAxis);
 
+      // Render the line.
       this.svg.append('path')
-        .datum(data)
+        .datum(this.data)
         .attr('class', 'line')
-        .attr('d', line);
+        .attr('d', this.line);
+
+    },
+
+
+    /**
+     * Construct axes and time-series line.
+     */
+    _initFocus: function() {
+
+      var self = this;
+
+      // Focus group.
+      this.focus = this.svg.append('g')
+        .attr('class', 'focus')
+        .style('display', 'none');
+
+      // Focus circle.
+      this.focus.append('circle')
+        .attr('r', 4);
+
+      // Cursor events target.
+      this.rect = this.svg.append('rect')
+        .attr('class', 'overlay')
+        .attr('width', this.width)
+        .attr('height', this.height);
+
+      // Show on hover.
+      this.rect.on('mouseover', function() {
+        self.focus.style('display', null);
+      });
+
+      // Hide on blur.
+      this.rect.on('mouseout', function() {
+        self.focus.style('display', 'none');
+      });
+
+      // Bisect on the X-axis.
+      var bisect = d3.bisector(function(d) {
+        return d[0];
+      });
+
+      // Focus.
+      this.rect.on('mousemove', function(e) {
+
+        // Get the nearest data point.
+        var x0 = self.xScale.invert(d3.mouse(this)[0]);
+        var i = bisect.left(self.data, x0, 1);
+        var d0 = self.data[i-1];
+        var d1 = self.data[i];
+        var d = x0 - d0[0] > d1[0] - x0 ? d1 : d0;
+
+        // Get the coordinates.
+        var x = self.xScale(d[0]);
+        var y = self.yScale(d[1]);
+
+        // Render the focus.
+        self.focus.attr('transform', 'translate('+x+','+y+')');
+
+      });
 
     }
 
