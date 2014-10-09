b3bafe39b49aff98de654804afc5050940345329
David McClure <davidwilliammcclure@gmail.com>
2014-10-08 17:48:59 -0700

    Adds chart container.

--- neatline/exhibits/themes/minard/text.php
+++ neatline/exhibits/themes/minard/text.php
@@ -23,3 +23,5 @@
 <h3 class="paris"><span data-neatline-slug="s3">Paris, November 20, 1869<span data-neatline-slug="s1">.</h2>
 
 <p><span data-neatline-slug="s4">The numbers of men present are represented by the widths of the colored zones at a rate of one millimeter for every ten-thousand men; they are further written across the zones</span>. <span data-neatline-slug="s5">The red designates the men who enter into Russia, the black those who leave it</span>. &mdash; <span data-neatline-slug="s6">The information which has served to draw up the map has been extracted from the works of M. M. Thiers, of Segur, of Fezensac, of Chambray, and the unpublished diary of Jacob, pharmacist of the army since October 28th</span>. <span data-neatline-slug="s7">In order to better judge with the eye the diminution of the army, I have assumed that the troops of prince Jerome and of Marshal Davoush who had been detached at Minsk and Moghilev and have rejoined around Orcha and Vitebsk, had always marched with the army</span>.</p>
+
+<div id="chart"></div>
